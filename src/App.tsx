6a0d45c0d508637c1dc96c9c4b6fd9471961069d
Jeanaica <jeanaica.suplido@gmail.com>
2021-12-16 18:36:02 +0800

    🎨 (#1): Modify app component
    Modify App and rename css files to scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="link1" element={<Link1 />} />
+        <Route path="link2" element={<Link2 />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <h1>Welcome to the app!</h1>
+      <nav>
+        <Link to="link1">Link 1</Link> | <Link to="link2">Link 2</Link>
+      </nav>
+      <div className="content">
+        <Outlet />
+      </div>
     </div>
   );
+}
+
+function Link1() {
+  return <h1>Link1</h1>;
+}
+
+function Link2() {
+  return <h1>Link2</h1>;
 }
 
 export default App;
