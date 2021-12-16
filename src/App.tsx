d2f3b1fc4ccc26fbe529e4ae7417f85379bbfc1b
Jeanaica <jeanaica.suplido@gmail.com>
2021-12-16 23:43:10 +0800

    🎨 Update App file
    Update App file and add initial landing page

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,14 @@ import { Routes, Route, Link, Outlet } from "react-router-dom";
 
 import "./App.scss";
 
+import Landing from "./pages/Landing";
+
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="link1" element={<Link1 />} />
-        <Route path="link2" element={<Link2 />} />
-      </Route>
+      <Route path="/" element={<Landing />} />
     </Routes>
   );
-}
-
-function Layout() {
-  return (
-    <div>
-      <h1>Welcome to the app!</h1>
-      <nav>
-        <Link to="link1">Link 1</Link> | <Link to="link2">Link 2</Link>
-      </nav>
-      <div className="content">
-        <Outlet />
-      </div>
-    </div>
-  );
-}
-
-function Link1() {
-  return <h1>Link1</h1>;
-}
-
-function Link2() {
-  return <h1>Link2</h1>;
 }
 
 export default App;
---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Landing: React.FC = (props) => {
+  return (
+    <div className="container">
+      <div className="buttons">
+        <button>We'll be there!</button>
+        <button>Not sure.</button>
+        <button>We can't come.</button>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
