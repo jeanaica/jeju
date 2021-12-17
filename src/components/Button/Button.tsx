6ba1587cfcb922b051be75c5431cdd20a9e04749
Jeanaica <jeanaica.suplido@gmail.com>
2021-12-17 12:44:45 +0800

    ✨ Add Landing page
    Add landing page

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Button.module.scss';
+
+const Button: React.FC = (props) => {
+  return (
+    <div>
+      Test content
+    </div>
+  );
+};
+
+export default Button;
--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 
+import styles from "./Landing.module.scss";
+
 const Landing: React.FC = (props) => {
   return (
-    <div className="container">
-      <div className="buttons">
-        <button>We'll be there!</button>
-        <button>Not sure.</button>
-        <button>We can't come.</button>
+    <div className={`${styles["landing"]}`}>
+      <div className={`${styles["container"]}`}>
+        <div className={`${styles["header"]}`}>
+          <h1 className="text--xxxl">Je | Ju</h1>
+          <span>April 1, 2022</span>
+        </div>
+        <div className={`${styles["buttons"]} col-7 col-s-9`}>
+          <button>We'll be there!</button>
+          <button>Not sure.</button>
+          <button>We can't come.</button>
+        </div>
       </div>
     </div>
   );
